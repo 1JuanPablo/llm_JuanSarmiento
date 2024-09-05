9b5f31de46c719c8ce3dfdeb419f51be82b7703b
Juan Pablo Sarmiento Arévalo <juanp.sarmientoa@ecci.edu.co>
2024-09-05 00:57:19 +0000

    UPDATE README.md

--- Ejemplos/web/ollama.js
+++ Ejemplos/web/ollama.js
@@ -8,5 +8,23 @@ function request () {
     };
 
     var url = "http://localhost:11434/api/generate"
-    alert("Hola");
+
+    var request = new XMLHttpRequest();
+
+    request.open('POST', URL, true);
+
+    request.setRequestHeader("Accept", "aplication/json")
+    request.setRequestHeader("Content-type", "aplication/json")
+    request.setRequestHeader("Acces-Control-Allow-Origin", 'http://localhost:8080');
+
+    request.send(JSON.stringify(datos));
+
+    request.onload = () => {
+        if (request.status == 200){
+            alert("RESPONSE");
+        }else{
+            alert("ERROR");
+        }
+    };
+
 };
